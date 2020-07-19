8021e58a0577ee140a440d6d802a73cb6840adb5
aditya <aditya.ramariya@gmail.com>
2020-07-19 11:43:07 +0530

    initial commit

---
+++ precache-manifest.1ae7f10c097ed6817eda9afe8d682667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6c2c35589ba0bac22c97721ad0182c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f44bcb83963833d77266",
+    "url": "/static/css/main.59b4ed80.chunk.css"
+  },
+  {
+    "revision": "ba83ffde78e4c543654f",
+    "url": "/static/js/2.5f00cad7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5f00cad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44bcb83963833d77266",
+    "url": "/static/js/main.9e6a0113.chunk.js"
+  },
+  {
+    "revision": "5291d243cc70d8ebd72a",
+    "url": "/static/js/runtime-main.a4a46b6e.js"
+  }
+]);
